e1fb570ed1d758c3564d985fc1a9b32cd6d201ab
Mark Odermatt <mark_odermatt@bluewin.ch>
2010-10-11 14:30:12 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameManager::Application.config.secret_token = 'f0e223efd62f089ad49f207672bbb372bcf5fbe88fa2cb4c7096f378d68f8f8685a54306f4c2ceb671c301aa753c8ec878b7c00f459ddecb03fca596ea254d08'
